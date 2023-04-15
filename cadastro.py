82b7e1478ff909abe30654b0829971ea27c6e525
Vanessa Santos <114835013+VanessaPro@users.noreply.github.com>
2023-04-15 15:08:24 -0300

    inserindo os dados para ler os dados e calcular  aposentadoria.

--- cadastro.py
+++ cadastro.py
@@ -4,4 +4,10 @@ dados['nome'] = str(input('Nome:'))
 nasc = int( input('Ano de nasimento: '))
 dados['idade'] = datetime.now().year - nasc
 dados['ctps'] = int(input('Carteira de trabalho(0 não tem):'))
-print(dados)
+if dados['ctps'] !=0:
+    dados['contratação'] = int(input('Ano de contratação: '))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+print('-=' * 30)
+for k, v in dados.items():
+    print(f' - {k} tem o valor {v}')
